1eca79eebb6545fd19f8e17e2b414764b952c7b5
Hudson8811 <hudson88@yandex.ru>
2020-07-01 10:59:14 +0300

    fix add person

--- app/js/main.js
+++ app/js/main.js
@@ -1,7 +1,4 @@
-
-
-
-$('.button-decor').click(function(){
+$('.button-decor').click(function () {
     $(".header__logo").addClass("header__logo--js-active")
 });
 
@@ -12,59 +9,76 @@ var party_style = new Swiper('.party-style .swiper-container', {
     //     el: '.swiper-pagination',
     //     type: 'fraction',
     //   },
-      navigation: {
+    navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-  });
+    },
+});
 
-  var party_style = new Swiper('.prizes__single-slider .swiper-container', {
+var party_style = new Swiper('.prizes__single-slider .swiper-container', {
     // pagination: {
     //     el: '.swiper-pagination',
     //     type: 'fraction',
     //   },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-  });
-
-
-
-  $('.party-personas__slider .swiper-container').each(function(){
-    var personas__slider = new Swiper(this, {
-     
     navigation: {
-        nextEl: $(this).parent().find('.swiper-button-next'),
-        prevEl: $(this).parent().find('.swiper-button-prev'),
-    },
-    
-
-  });
-  });
-
-  var party_style = new Swiper('.prizes__three-slider .swiper-container', {
-    slidesPerView:2,
-      navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
-      },
-      breakpoints: {
-          1024: {
-              slidesPerView: 3,
-          },
-      }
-  });
+    },
+});
+
+var personas__slider=[];
+
+$('.party-personas__slider .swiper-container').each(function ( index, value) {
+    personas__slider[index] = new Swiper(value, {
+
+        navigation: {
+            nextEl: $(value).parent().find('.swiper-button-next'),
+            prevEl: $(value).parent().find('.swiper-button-prev'),
+        },
+
+
+    });
+});
+
+$('.party-personas__image-container--add').click(function () {
+    if ($('.party-personas__slider--active').length < 6) {
+        var index = $('.party-personas__slider:not(.party-personas__slider--active):not(.party-personas__slider--add):first')
+        .addClass('party-personas__slider--active')
+        .index();
+        setTimeout(function () {
+            personas__slider[index].update();
+            //personas__slider.destroy(false);
+            //personas__slider.init();
+        }, 700);
+
+    }
+    console.log($('.party-personas__slider--active:not(.party-personas__slider--add)').length);
+    if ($('.party-personas__slider--active:not(.party-personas__slider--add)').length == 6) {
+        $('.party-personas__slider--add').removeClass('party-personas__slider--add');
+    }
+});
+var party_style = new Swiper('.prizes__three-slider .swiper-container', {
+    slidesPerView: 2,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+        1024: {
+            slidesPerView: 3,
+        },
+    }
+});
 
 
 //   $('.prizes__single-slider .swiper-container').each(function(){
 //     var prizes__single_slider = new Swiper(this, {
-     
+
 //     navigation: {
 //         nextEl: $(this).parent().find('.swiper-button-next'),
 //         prevEl: $(this).parent().find('.swiper-button-prev'),
 //     },
-    
+
 
 //   });
 //   });
@@ -72,11 +86,11 @@ var party_style = new Swiper('.party-style .swiper-container', {
 //   document.getElementById("pct").addEventListener("change", function () {
 //     if (this.files[0]) {
 //       var fr = new FileReader();
-  
+
 //       fr.addEventListener("load", function () {
 //         document.getElementById("label-for-pct").style.backgroundImage = "url(" + fr.result + ")";
 //       }, false);
-  
+
 //       fr.readAsDataURL(this.files[0]);
 //     }
 //   });
@@ -89,7 +103,7 @@ new WOW().init();
 
 
 var receptsSlider = new Swiper('.recepts__slider .swiper-container', {
-    slidesPerView:1,
+    slidesPerView: 1,
     effect: 'fade',
     loop: true,
     simulateTouch: false,
@@ -99,7 +113,7 @@ var receptsSlider = new Swiper('.recepts__slider .swiper-container', {
     },
 });
 
-jQuery(document).ready(function(){
+jQuery(document).ready(function () {
     jQuery('.scrollbar-macosx').scrollbar();
     $('#recepts').addClass('hide');
 });
@@ -118,8 +132,8 @@ $('.js-show-recepts').click(function () {
 
 
 
-$(document).ready(function(){
-    function colRec(){
+$(document).ready(function () {
+    function colRec() {
         $('.recepts__left').each(function () {
             $(this).find('.recepts__col-2 .recepts__ind').addClass('moved');
             var right = $(this).find('.recepts__col-1')
@@ -127,7 +141,7 @@ $(document).ready(function(){
         });
     };
 
-    function colRecBack(){
+    function colRecBack() {
         $('.recepts__left').each(function () {
             var right = $(this).find('.recepts__col-2')
             $(this).find('.moved').appendTo(right);
@@ -148,8 +162,8 @@ $(document).ready(function(){
     }
 
 
-    $(window).on('resize', function(){
-        if ($(window).width() <= 1024){
+    $(window).on('resize', function () {
+        if ($(window).width() <= 1024) {
             if (!plansSlider) {
                 plansSlider = $('.catalog__slider').slick({
                     arrows: true,
@@ -176,6 +190,8 @@ $(document).ready(function(){
     });
 
     $('.js-dot').click(function () {
-       $(this).closest('.dot').toggleClass('active');
+        $(this).closest('.dot').toggleClass('active');
     });
+
+
 });
